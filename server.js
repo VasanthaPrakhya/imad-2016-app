1a5abe54a0f1f2a6266d9c7141be7e4d4f789280
VasanthaPrakhya <prakhya.vasantha@gmail.com>
2016-10-21 15:59:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile('Article-one has been requested and will be served here');
+    res.sendFile("Article-one has been requested and will be served here");
     
 });
 
